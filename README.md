dc01350f7d5c801ec58b0b32ce10556e8971bbc6
Posivnich <lposivnich@code-pilots.ru>
2019-11-30 10:52:02 +0300

    init state

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # node-rest-api
 lesson example
+
+
+## Before start
+Install PostgresSQL
+
+```
+sudo apt update
+sudo apt install postgresql postgresql-contrib
+```
+
+Login to postgres
+```
+sudo -u postgres psql
+```
+
+Create user and password
+```
+CREATE ROLE api_user WITH LOGIN PASSWORD 'password';
+ALTER ROLE api_user CREATEDB;
+```
+
+Logout of the root
+```
+\q
+psql -d postgres -U api_user
+```
+
+Create db
+```
+CREATE DATABASE books_api;
+\c books_api
+```
+
+Create table
+```
+CREATE TABLE books (
+  ID SERIAL PRIMARY KEY,
+  author VARCHAR(255) NOT NULL,
+  title VARCHAR(255) NOT NULL
+);
+```
+Insert
+```
+INSERT INTO books (author, title)
+VALUES  ('J.K. Rowling', 'Harry Potter');
+```
---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { pool } = require('./config');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+const getBooks = (request, response) => {
+    pool.query('SELECT * FROM books', (error, result) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).json(result.rows)
+    })
+};
+
+const addBook = (request, response) => {
+    const { author, title } = request.body;
+
+    pool.query('INSERT INTO books (author, title) VALUES ($1, $2)', [author, title], error => {
+        if (error) {
+            throw error;
+        }
+        response.status(201).json({status: 'success', message: 'Book added.'})
+    })
+};
+
+app
+    .route('/books')
+    .get(getBooks)
+    .post(addBook);
+
+app.listen(process.env.PORT || 3002, () => {
+    console.log('Server listening')
+});
