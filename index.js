dcd7507d2a6178edf9f49be4bc7c7810a46ab71f
Posivnich <lposivnich@code-pilots.ru>
2019-12-01 10:21:43 +0300

    upd server config, add delete method; g

--- index.js
+++ index.js
@@ -3,11 +3,29 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const { pool } = require('./config');
 
+const helmet = require('helmet');
+const compression = require('compression');
+const rateLimit = require('express-rate-limit');
+const { body, check, validationResult } = require('express-validator');
+
+const isProduction = process.env.NODE_ENV === 'production';
+const origin = {
+    origin: isProduction ? 'https://exampla-node-api.herokuapp.com/' : '*',
+};
+
+const limiter = rateLimit({
+    windowMs: 60 * 1000,
+    max: 5,
+});
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors(origin));
+app.use(compression());
+app.use(helmet());
+app.use(limiter);
 
 const getBooks = (request, response) => {
     pool.query('SELECT * FROM books', (error, result) => {
@@ -19,6 +37,12 @@ const getBooks = (request, response) => {
 };
 
 const addBook = (request, response) => {
+    const errors = validationResult(request);
+
+    if (!errors.isEmpty()) {
+        return response.status(422).json({ error: errors.array() });
+    }
+
     const { author, title } = request.body;
 
     pool.query('INSERT INTO books (author, title) VALUES ($1, $2)', [author, title], error => {
@@ -29,10 +53,43 @@ const addBook = (request, response) => {
     })
 };
 
+const deleteBook = (request, response) => {
+    if (!request.header('apiKey') || request.header('apiKey') !== process.env.API_KEY) {
+        return response.status(401).json({ status: 'error', message: 'Unauthorized'})
+    }
+
+    const { id } = request.body;
+
+    pool.query('DELETE FROM books WHERE id = $1', [id], error => {
+        if (error) {
+            throw error;
+        }
+        response.status(201).json({status: 'success', message: 'Book deleted.'})
+    })
+};
+
 app
     .route('/books')
     .get(getBooks)
-    .post(addBook);
+    .delete(deleteBook);
+
+const postLimiter = rateLimit({
+    windowMs: 60 * 1000,
+    max: 3,
+});
+const validationAddBook = [
+    check('author')
+        .not()
+        .isEmpty()
+        .isLength({ min:5, max: 255})
+        .trim(),
+    check('title')
+        .not()
+        .isEmpty()
+        .isLength({ min:5, max: 255})
+        .trim()
+];
+app.post('/books', validationAddBook, postLimiter, addBook);
 
 app.listen(process.env.PORT || 3002, () => {
     console.log('Server listening')
